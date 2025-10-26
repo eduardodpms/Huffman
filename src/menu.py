e4cd9e5622b2a4a67cdaf62202edf208848f5b0d
eduardodpms <eduardopms19@hotmail.com>
2025-10-26 04:31:40 -0300

    feat: add janela de carregamento

--- src/menu.py
+++ src/menu.py
@@ -1,7 +1,9 @@
 from src.huffman import char_count, prefix_tree, prefix_codes
 from bitarray import bitarray
-from tkinter import filedialog, messagebox
+from tkinter import filedialog, messagebox, ttk
 import tkinter as tk
+import threading
+import time
 import math
 import json
 import os
@@ -42,7 +44,9 @@ class App():
         btn_exit.grid(row=3, column=1, padx=5, pady=5)
         checkbox.grid(row=4, column=1, padx=5, pady=(16, 0))
 
+
 ################################################################################################################################
+
 
     # Abre e lê o arquivo, e salva a compressão em um binário
     def on_compress(self):
@@ -70,6 +74,8 @@ class App():
         # Obter sequências binárias para o header e o conteúdo
         try:
             bin_header, bin_content = self.compression(filepath, content)
+            if self.cancel_flag:
+                return
         except Exception as e:
             messagebox.showerror("Erro", f"Não foi possível codificar o conteúdo:\n{e}")
             return
@@ -93,22 +99,23 @@ class App():
                 f'Comprimido: {len(bin_header) + math.ceil(len(bin_content)/8)} bytes'
         messagebox.showinfo("Compressão Finalizada", f"> Conteúdo salvo em:\n{outpath}\n\n> Tamanho dos Arquivos:\n{sizes}")
 
-    # Codifica a sequêcia de caracteres do arquivo desejado e gera um header
+
+    # Gera um header e codifica a sequêcia de caracteres do arquivo desejado
     def compression(self, filepath, content):
         # Obter os prefix codes
         chars = char_count(content)
         n, tree = prefix_tree(chars)
         prefix = prefix_codes(tree)
 
-        # Serializar o conteúdo
-        bin_content = bitarray('')
-        for char in content:
-            bin_content += bitarray(prefix[char])
+        # Codificar o conteúdo, exibindo uma barra de progresso
+        threading.Thread(target=self.encode, args=(content, prefix)).start()
+        self.loading_window()
+        self.lw.destroy()
 
         # Montar o header
         header = {
             'header': 0, # Quantidade de bytes do header codificado
-            'content': math.ceil(len(bin_content)/8), # Quantidade de bytes do conteúdo codificado
+            'content': math.ceil(len(self.temp)/8), # Quantidade de bytes do conteúdo codificado
             'string': n, # Quantidade de caracteres do conteúdo original
             'ext': os.path.splitext(filepath)[1], # Entensão do arquivo original
             'bin': 0 if type(content) == str else 1, # Se o arquivo original foi lido como binário
@@ -127,14 +134,37 @@ class App():
         bin_header = b'{"header": ' + size_bytes + bin_header[12:] # Dicionário serializado, com seu próprio tamanho
         
         # Visualizar o header
-        if(self.header_bool.get()):
+        if(self.header_bool.get() and not self.cancel_flag):
             print(f'{'='*64}\n{int((64-len(f'header for {os.path.basename(filepath)}'))/2)*' '}' \
                   f'{f'header for {os.path.basename(filepath)}'}\n\n{header}\n{'='*64}')
         
         # Retornar header e conteúdo serializados
-        return bin_header, bin_content
+        return bin_header, self.temp
+
+
+    # Codifica a sequêcia de caracteres do conteúdo lido
+    def encode(self, content, prefix):
+        bin_content = bitarray('')
+        self.cancel_flag = False
+
+        time.sleep(0.1)
+
+        for char in content:
+            bin_content += bitarray(prefix[char])
+
+            self.bar.step(100/len(content))
+            self.label.config(text=f"Comprimindo Sequência: {self.bar['value']:.1f}%")
+            self.lw.update_idletasks() # Atualiza a interface
+            
+            if self.cancel_flag:
+                break
+        
+        self.temp = bin_content
+        self.lw.quit()
+
 
 ################################################################################################################################
+
 
     # Abre e lê o arquivo binário, e salva a descompressão em outro arquivo
     def on_decompress(self):
@@ -159,10 +189,12 @@ class App():
         # Obter o header e o conteúdo desserializados
         try:
             header, content = self.decompression(bits)
+            if self.cancel_flag:
+                return
         except Exception as e:
             messagebox.showerror("Erro", f"Não foi possível decodificar a sequência binária:\n{e}")
             return
-        
+
         # Visualizar o header
         if(self.header_bool.get()):
             print(f'{'='*64}\n{int((64-len(f'header for {os.path.basename(filepath)}'))/2)*' '}' \
@@ -189,33 +221,59 @@ class App():
         sizes = f'Comprimido: {int(len(bits)/8)} bytes\n' \
                 f'Descomprimido: {os.path.getsize(outpath)} bytes'
         messagebox.showinfo("Descompressão Finalizada", f"> Conteúdo salvo em:\n{outpath}\n\n> Tamanho dos Arquivos:\n{sizes}")
-    
+
+
     # Decodifica a sequêcia de binários do arquivo desejado
     def decompression(self, bits):
-        # Recuperar a quantidade de bytes do header e depois o header
+        # Recuperar a quantidade de bytes do header
         header_bytes_end = bits.index(bitarray(b', '))
         header_bytes = int(bits[88:header_bytes_end].tobytes())
-        header = json.loads(bits[:header_bytes*8].tobytes())
 
-        # Recuperar o conteúdo
-        content_bits = bits[header_bytes*8:] # Recupera os bits do conteúdo
-        prefix = {v: k for k, v in header['prefix'].items()} # Dicionário invertido de prefixos, para facilitar acessos
-        content, current_prefix = '', '' # String para o conteúdo e string de bits para procura de prefixos
+        # Recuperar o header e os bits do conteúdo
+        header = json.loads(bits[:header_bytes*8].tobytes())
+        content_bits = bits[header_bytes*8:]
+
+        # Recuperar o conteúdo, exibindo uma barra de progresso
+        threading.Thread(target=self.decode, args=(content_bits, header)).start()
+        self.loading_window()
+        self.lw.destroy()
+        
+        # Retornar header e conteúdo desserializados
+        return header, self.temp
+
+
+    # Decodifica os bits do conteúdo com base nos códigos prefixos
+    def decode(self, content_bits, header):
+        prefix = {v: k for k, v in header['prefix'].items()}
+        content, current_prefix = '', ''
+        self.cancel_flag = False
+
+        time.sleep(0.1)
 
         for bit in content_bits: # Itera pelos bits, procurando correspondências no dicionário de prefixos
             current_prefix += f'{bit}'
+
             if(current_prefix in prefix.keys()):
                 content += prefix[current_prefix]
                 current_prefix = ''
 
+                self.bar.step(100/header['string'])
+                self.label.config(text=f"Descomprimindo Sequência: {self.bar['value']:.1f}%")
+                self.lw.update_idletasks() # Atualiza a interface
+            
+            if self.cancel_flag:
+                break
+
         # Remover caracteres extras causados pela extensão de bits do Bitarray
         while(len(content) > header['string']):
             content = content[:-1]
-        
-        # Retornar header e conteúdo desserializados
-        return header, content
+            
+        self.temp = content
+        self.lw.quit()
+
 
 ################################################################################################################################
+
 
     # Retorna uma string com o path do arquivo de saída (e resolve conflitos)
     def outpath(self, filepath, outfolder, id, ext):
@@ -236,3 +294,27 @@ class App():
                 title="Salvar como",
                 filetypes=[("All files", f"*{ext}")]
             ) if not resp else outpath
+    
+
+    # Abre uma janela de carregamento para as operações de codificação e decodificação
+    def loading_window(self):
+        self.lw = tk.Toplevel(self.root)
+        self.lw.title("Progresso")
+        self.lw.geometry("274x114")
+        self.lw.resizable(False, False)
+
+        self.label = tk.Label(self.lw, text="")
+        self.bar = ttk.Progressbar(self.lw, length=250)
+        lw_cancel = tk.Button(self.lw, text="Cancelar", width=12, command=self.loading_cancel)
+
+        self.label.pack(pady=10)
+        self.bar.pack()
+        lw_cancel.pack(padx=12, pady=12, anchor='se')
+
+        self.lw.grab_set()
+        self.lw.mainloop()
+
+
+    # Gera uma caixa de confirmação para cancelar o processamento
+    def loading_cancel(self):
+        self.cancel_flag = messagebox.askyesno("Cancelar?", f"Deseja cancelar a operação?")
